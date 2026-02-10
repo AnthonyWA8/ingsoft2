31bc2e408398c3a02932b0274e87801ba166573d
Anthony <sebastianagva@gmail.com>
2026-02-10 17:30:38 -0500

    Main-Repaso

---
+++ src/Repaso.java
@@ -0,0 +1,17 @@
+
+
+public class Repaso {
+
+    public static void main(String[]args){
+
+       Bibliotecario bibliotecario = new Bibliotecario("1", "juan", "jajajaj@gmail.com", "noche");
+       System.out.println(bibliotecario.darEmail());
+
+
+    }
+
+
+
+
+
+}
