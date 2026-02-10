78a5e3138e4c29cd7e213fb14c4a1a85c00481d7
Anthony <sebastianagva@gmail.com>
2026-02-10 17:45:36 -0500

    agregacion socio

---
+++ src/Socio.java
@@ -0,0 +1,30 @@
+public class Socio extends Usuario{
+
+
+    private int limiteEjemplares;
+    private int ejemplaresEnprestamo;
+    private double multasPendientes;
+
+
+    public Socio(String id, String nombre, String email){
+        super(id, nombre, email);
+        this.limiteEjemplares = limiteEjemplares;
+        ejemplaresEnprestamo = 0;
+        multasPendientes = 0;
+
+    }
+
+    public void  registrarPrestamo(int cantidad){
+        if (cantidad > limiteEjemplares){
+            throw new IllegalArgumentException("supera el limite socio");
+        }
+
+        if (multasPendientes > 0) {
+            throw new IllegalArgumentException("tiene multas, no puede pedir mas");
+        }
+
+        System.out.println("se prestaron" + cantidad + "libros");
+
+        }
+
+}
