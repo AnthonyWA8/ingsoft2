bf948674d447785e79ba06f99d750fa55bc787d3
Anthony <sebastianagva@gmail.com>
2026-02-10 17:18:23 -0500

    Clase bibliotecario

---
+++ src/Bibliotecario.java
@@ -0,0 +1,13 @@
+public class Bibliotecario extends Usuario{
+
+    private String turno;
+
+    public Bibliotecario(String id, String nombre, String email, String turno){
+        super(id, nombre, email);
+        this.turno = turno;
+    }
+
+    public String darTurno(){
+        return this.turno;
+    }
+}
